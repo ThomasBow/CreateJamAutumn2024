024063861297a40e6f0620ae288d557357939aa1
Patrick <patrick@reiffenste.in>
2024-11-08 23:19:01 +0100

    changes

---
+++ Godot Project/scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using Godot;
+
+public partial class PlayerMovement : CharacterBody2D
+{
+	[Export]
+	public int speed = 300;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+	
+	public void CalculateMovement() {
+		// Get direction
+		Vector2 directionVector = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		// Calculate movement vector
+		Velocity += directionVector * speed;
+	}
+	
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _PhysicsProcess(double delta)
+	{
+		CalculateMovement();
+		MoveAndSlide(); //This function uses delta itself
+	}
+}
