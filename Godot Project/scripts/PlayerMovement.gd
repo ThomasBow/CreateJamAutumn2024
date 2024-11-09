1ada2129eda60b6e9a23f3b3192a4ef58c5b9714
ThomasBow <101808093+ThomasBow@users.noreply.github.com>
2024-11-09 17:52:06 +0100

    highlighting work, and tile set utilities

--- Godot Project/scripts/PlayerMovement.gd
+++ Godot Project/scripts/PlayerMovement.gd
@@ -8,6 +8,9 @@ class_name PlayerMovement
 var tile_map_layers: Array = []
 var _animated_sprite_2d: AnimatedSprite2D
 
+enum Direction { UP, DOWN, LEFT, RIGHT }
+var lastDirection: Direction = Direction.LEFT
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_animated_sprite_2d = $AnimatedSprite2D
@@ -20,23 +23,54 @@ func get_tile_map_layers() -> void:
 			tile_map_layers.append(child)
 
 # Function to get the tile(s) the player is currently standing on
-func get_current_tiles_from_position() -> TileData:
-	var player_position = global_position
-	var layers_i = tile_map_layers.count(TileMapLayer) - 1
-	for tile_map_layer_i in range(layers_i, 0, -1):
-		var tile_map_layer = tile_map_layers[tile_map_layer_i]
-		return get_current_tile_from_layer(tile_map_layer)
-	return null
+func get_current_tiles_from_position() -> Array:
+	var playerPosition = global_position
+	var tileDatas = []
+	var layers_i = tile_map_layers.size() - 1
+	for tile_map_layer_i in range(layers_i, -1, -1):
+		var tile_map_layer: TileMapLayer = tile_map_layers[tile_map_layer_i]
+		var tileData = get_current_tile_from_layer(playerPosition, tile_map_layer)
+		if tileData != null:
+			tileDatas.append(tileData)
+	return tileDatas
 
 # Function to get the tile from the given layer
-func get_current_tile_from_layer(layer: TileMapLayer) -> TileData:
-	var player_position = global_position
-	var tile_coords = layer.local_to_map(layer.to_local(player_position))
-	return layer.get_cell_tile_data(tile_coords)
+func get_current_tile_from_layer(playerPosition: Vector2, layer: TileMapLayer) -> TileData:
+	var tileCoords = layer.local_to_map(playerPosition)
+	var tileData = layer.get_cell_tile_data(tileCoords)
+	return tileData
+
+func get_neighboring_tile(player_position: Vector2, layer: TileMapLayer, direction: Direction) -> TileData:
+	var current_tile_coords = layer.local_to_map(player_position)
+	var neighbor_coords = current_tile_coords
+	
+	# Adjust the coordinates based on direction
+	match direction:
+		Direction.UP:
+			neighbor_coords.y -= 1  # Move up in the grid
+		Direction.DOWN:
+			neighbor_coords.y += 1  # Move down in the grid
+		Direction.LEFT:
+			neighbor_coords.x -= 1  # Move left in the grid
+		Direction.RIGHT:
+			neighbor_coords.x += 1  # Move right in the grid
+
+	# Get tile data at the new coordinates
+	var tile_data = layer.get_cell_tile_data(neighbor_coords)
+	return tile_data
 
 func calculate_movement() -> void:
 	# Get direction
 	var direction_vector: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	if direction_vector.x > 0:
+		lastDirection = Direction.RIGHT
+	elif direction_vector.x < 0:
+		lastDirection = Direction.LEFT
+	elif direction_vector.y > 0:
+		lastDirection = Direction.UP
+	else:
+		lastDirection = Direction.DOWN
 	
 	# Flip the sprite based on direction
 	if direction_vector.x < 0:
@@ -47,11 +81,21 @@ func calculate_movement() -> void:
 	# Calculate movement vector
 	velocity = direction_vector * speed
 
+func GetLayerWithName(layerName: String) -> TileMapLayer:
+	for layer in tile_map_layers:
+		if layer.name == layerName:
+			return layer 
+	return null
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	calculate_movement() # This function uses delta itself
 	move_and_slide()
-	var tileData = get_current_tiles_from_position()
-	if tileData != null:
-		var terrain_s = String.num_int64(tileData.terrain)
-		print(terrain_s)
+	
+	var highlightLayer = GetLayerWithName("HighlightLayer")
+	get_neighboring_tile(global_position, highlightLayer, lastDirection)
+	var tileDatas = get_current_tiles_from_position()
+	print("----------------")
+	if tileDatas.size() > 0:
+		for tileData in tileDatas:
+			print(tileData.to_string())
