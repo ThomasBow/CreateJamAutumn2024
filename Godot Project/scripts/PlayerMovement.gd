93d5ad6e040062cc13e9582837c5e00838bce446
ThomasBow <101808093+ThomasBow@users.noreply.github.com>
2024-11-09 15:08:09 +0100

    Trying to get useful data from the tile map

--- Godot Project/scripts/PlayerMovement.gd
+++ Godot Project/scripts/PlayerMovement.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+class_name PlayerMovement
+
 @export var speed: int = 300
 @export var tile_map_layer_parent_node: Node2D
 
@@ -16,6 +18,20 @@ func get_tile_map_layers() -> void:
 	for child in tile_map_layer_parent_node.get_children():
 		if child is TileMapLayer:
 			tile_map_layers.append(child)
+
+# Function to get the tile(s) the player is currently standing on
+func get_current_tiles_from_position()
+	var player_position = global_position
+	var layers_i = tile_map_layers.count() - 1
+	for tile_map_layer_i in range(layers_i, 0, -1):
+		var tile_map_layer = tile_map_layers[tile_map_layer_i]
+		
+
+# Function to get the tile from the given layer
+func get_current_tile_from_layer(layer: TileMapLayer) -> TileData:
+	var player_position = global_position
+	var tile_coords = layer.local_to_map(layer.to_local(player_position))
+	return layer.get_cell_tile_data(tile_coords)
 
 func calculate_movement() -> void:
 	# Get direction
@@ -34,3 +50,4 @@ func calculate_movement() -> void:
 func _physics_process(delta: float) -> void:
 	calculate_movement() # This function uses delta itself
 	move_and_slide()
+	print(get_current_tile().terrain)
